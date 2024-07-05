3b9dc72db7897158297e1f2edc1dcee18acb9dad
Кристияна Николова <K.Nikolova21g@pmgrz.net>
2024-07-05 10:58:48 +0300

    napravih get set na purvite 3 promenlivi

--- Hotel.cs
+++ Hotel.cs
@@ -6,8 +6,65 @@ using System.Threading.Tasks;
 
 namespace KrisiTediPraktika10g
 {
-    class Hotel
+    public class Hotel
     {
+        private int roomNumber;
+        private string type;
+        private int capacity;
 
+        public int RoomNumber
+        {
+            get
+            {
+                return roomNumber;
+            }
+            private set
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException("Номерът на стаята трябва да е положителен и по-голям от 0!");
+                }
+                else
+                {
+                    roomNumber = value;
+                }
+            }
+        }
+        public string Type 
+        {    
+            get
+            {
+                return type;
+            }
+            private set
+            {
+                if (type=="")
+                {
+                    throw new ArgumentException("Полето с типа на стаята не може да е празно!");
+                }
+                else
+                {
+                    type = value;
+                }
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            private set
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("Капацитетът на стаюта трябва да е по-голям от 0!");
+                }
+                else
+                {
+                    capacity = value;
+                }
+            }
+        }
     }
 }
